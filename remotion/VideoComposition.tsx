a1c5ec6b770a0e498ae771f89ce386466d740a66
Chatchai-29 <221957267+Chatchai-29@users.noreply.github.com>
2026-08-26 16:02:17 +1000

    captions: raise position another 40% (372 -> 521)

--- remotion/VideoComposition.tsx
+++ remotion/VideoComposition.tsx
@@ -116,7 +116,7 @@ const Captions: React.FC<{ words?: Word[] }> = ({ words }) => {
           justifyContent: "center",
           gap: "0 19px",
           maxWidth: "88%",
-          marginBottom: 372,
+          marginBottom: 521,
         }}
       >
         {chunk.map((w, i) => {
